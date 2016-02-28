0c5dd97f9775974d2480b78f8057d9988cd6098a
Val Booth <vxb4825@rit.edu>
2016-02-27 20:19:58 -0500

    more println debugging

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ cursor = db.cursor()
 @app.route('/newUser/', methods=['POST'])
 def newUser():
 	print("made it to the function")
-	print(str(request[0]))
+	print(request)
 	firstName = request.form['firstName']
 	print(firstName)
 	lastName = request.form['lastName']
