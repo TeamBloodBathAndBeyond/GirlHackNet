16fba8e7a90eeabf41ae28fd3911070e6c05dac9
noblelady <cnoble9196@gmail.com>
2016-02-28 08:36:20 -0500

    fixed a button issue

--- js/popup.js
+++ js/popup.js
@@ -10,10 +10,10 @@ function check_empty() {
 //Function To Display Popup
 function div_show() {
   document.getElementById('pop-up').style.display = "block";
-  console.log('showing');
+  // console.log('showing');
 }
 //Function to Hide Popup
 function div_hide(){
   document.getElementById('pop-up').style.display = "none";
-  console.log('hiding');
+  //console.log('hiding');
 }
