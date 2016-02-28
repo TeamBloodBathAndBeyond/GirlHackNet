29940e08374bf32e5619415f182914d47dd216a9
Val Booth <vxb4825@rit.edu>
2016-02-27 20:23:01 -0500

    trying a different way to get the post data

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ cursor = db.cursor()
 @app.route('/newUser/', methods=['POST'])
 def newUser():
 	print("made it to the function")
-	print(str(request.get_data()))
+	print(str(request.get_data().json))
 	firstName = request.form['firstName']
 	print(firstName)
 	lastName = request.form['lastName']
