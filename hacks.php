7e402b7c7500601f4afa43b80f01f000ee61c9e0
noblelady <cnoble9196@gmail.com>
2016-02-27 21:00:34 -0500

    using get now

--- hacks.php
+++ hacks.php
@@ -14,32 +14,7 @@
     <h3>Welcome  ADD PERSON'S NAME HERE</h3>
   </div>
   <div id='main'>
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "girlhack";
-    $dbname = "GirlHack_DB";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "SELECT date, link, name, location FROM getEvents";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-        echo "date: " . $row["date"]. " - Link: " . $row["link"]. " Name:" . $row["name"]. " Location:" . $row["location"]. "<br>";
-      }
-    } else {
-      echo "0 results";
-    }
-    $conn->close();
-    ?>
+<?php $json = http_get("localhost:800/getEvents"); ?>
   </main>
 </body>
 </html>
