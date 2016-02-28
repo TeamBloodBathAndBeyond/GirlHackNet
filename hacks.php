0cb100b9d2a798bd0a3540a2fc69b3e69cfdc8b8
noblelady <cnoble9196@gmail.com>
2016-02-27 20:33:27 -0500

    testing out the info from the JSON file

--- hacks.php
+++ hacks.php
@@ -6,32 +6,7 @@
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="icon" href="imgs/miniLogo.jpg" type="image/jpg" sizes="16x16">
 
-  <?php
-  $servername = "localhost";
-  $username = "root";
-  $password = "girlhack";
-  $dbname = "GirlHack_DB";
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  $sql = "SELECT id, firstname, lastname FROM MyGuests";
-  $result = $conn->query($sql);
-
-  if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
-  $conn->close();
-  ?>
 </head>
 <body>
   <div id='header'>
@@ -39,6 +14,32 @@
     <h3>Welcome  ADD PERSON'S NAME HERE</h3>
   </div>
   <div id='main'>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "girlhack";
+    $dbname = "GirlHack_DB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT date, link, name, location FROM getEvents";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+        echo "date: " . $row["date"]. " - Link: " . $row["link"]. " Name:" . $row["name"]. " Location:" . $row["location"]. "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+    $conn->close();
+    ?>
   </main>
 </body>
 </html>
